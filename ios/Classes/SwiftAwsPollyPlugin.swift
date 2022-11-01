ad69218def8ee4f3443d412e60c8bf3d579880b0
lookx2 <34668401+lookx2@users.noreply.github.com>
2022-11-01 22:48:50 +0800

    Update SwiftAwsPollyPlugin.swift

--- ios/Classes/SwiftAwsPollyPlugin.swift
+++ ios/Classes/SwiftAwsPollyPlugin.swift
@@ -52,7 +52,7 @@ public class SwiftAwsPollyPlugin: NSObject, FlutterPlugin {
       // Text to synthesize
       input.text = text
       
-      input.engine = AWSPollyEngine.AWSPollyEngineNeural
+      input.engine = AWSPollyEngine.eural
 
       // We expect the output in MP3 format
       input.outputFormat = AWSPollyOutputFormat.mp3
